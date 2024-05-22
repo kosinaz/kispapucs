4adcef8ed220297558a473ea1a817b678870f5c2
Kosina Zoltán <kosinaz@gmail.com>
2024-05-22 16:38:09 +0200

    update

--- Kispapucs/game.gd
+++ Kispapucs/game.gd
@@ -26,3 +26,14 @@ func _process(_delta):
 		$Lookout2.show()
 	if Input.is_action_pressed("ui_cancel"):
 		get_tree().quit()
+	if Input.is_action_just_released("ui_select"):
+		if $Skycycle.visible:
+			if abs($Skycycle.position.x - $Skye.position.x) < 100:
+				$Skycycle.hide()
+				$Skye.get_node("Cycle").show()
+				$Skye.position.y = 420
+		else:
+			$Skycycle.position.x = $Skye.position.x
+			$Skycycle.show()
+			$Skye.get_node("Cycle").hide()
+			$Skye.position.y = 520
--- Kispapucs/skye.gd
+++ Kispapucs/skye.gd
@@ -15,12 +15,24 @@ func _ready():
 func _process(delta):
 	if Input.is_action_pressed("ui_right"):
 		position.x += 300 * delta
-		$AnimatedSprite.play("right")
+		if not $Cycle.visible:
+			$AnimatedSprite.play("right")
+		else:
+			$Cycle.flip_h = false
 		$AnimatedSprite.flip_h = false
 	elif Input.is_action_pressed("ui_left"):
 		position.x -= 300 * delta
-		$AnimatedSprite.play("right")
+		if not $Cycle.visible:
+			$AnimatedSprite.play("right")
+		else:
+			$Cycle.flip_h = true
 		$AnimatedSprite.flip_h = true
 	else:
 		$AnimatedSprite.play("default")
+	if Input.is_action_pressed("ui_up"):
+		if $Cycle.visible:
+			position.y -= 300 * delta
+	elif Input.is_action_pressed("ui_down"):
+		if $Cycle.visible:
+			position.y += 300 * delta
 		
